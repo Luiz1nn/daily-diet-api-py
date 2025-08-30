f8bd0894ab6cc107c8abf202d2135e4a27eb8dd9
Luiz1nn <luisfernandobr2003@gmail.com>
2025-08-30 17:50:58 -0300

    ✨ feat: add login and logout endpoints with user authentication

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 import bcrypt
 from models.user import User
 from database import db
@@ -16,7 +16,29 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-  return User.query.get(user_id)
+    return User.query.get(user_id)
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Autenticação realizada com sucesso!"})
+
+    return jsonify({"message": "Credenciais inválidas"}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
 
 @app.route('/user', methods=["POST"])
 def create_user():
