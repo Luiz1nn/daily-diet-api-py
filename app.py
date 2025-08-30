fae149e3504bf0059625de64cb401645817c9a31
Luiz1nn <luisfernandobr2003@gmail.com>
2025-08-30 17:29:05 -0300

    ✨ feat: integrate Flask-Login with user session management

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_login import LoginManager
+from models.user import User
 from database import db
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin123@127.0.0.1:3306/daily-diet-db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
 
 if __name__ == '__main__':
   app.run(debug=True)
