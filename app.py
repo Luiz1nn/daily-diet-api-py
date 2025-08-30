6a75f123f19817a4632dabd27c17b6c7d804b1a2
Luiz1nn <luisfernandobr2003@gmail.com>
2025-08-30 17:09:41 -0300

    ✨ feat: integrate MySQL database with Flask app

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from database import db
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin123@127.0.0.1:3306/flask-crud'
+
+db.init_app(app)
 
 if __name__ == '__main__':
   app.run(debug=True)
